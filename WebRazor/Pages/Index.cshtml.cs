666da0c9c8bcc68b7176b002ee96bb4e47155680
zemlianin <detulie@mail.ru>
2022-09-18 09:30:08 +0300

    скачивание

--- WebRazor/Pages/Index.cshtml.cs
+++ WebRazor/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.Text.Json;
+using System.Text.Json;
 namespace WebRazor.Pages
 {
     public class IndexModel : PageModel
@@ -12,8 +13,13 @@ namespace WebRazor.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var request = new RequestSender();
+
+            ViewData["Bytes"] = JsonSerializer.Deserialize<Byte[]>(await request.Get("http://APILoad/get-app-executable?services=winget%20install%20Notepad%2B%2B.Notepad%2B%2B"));
+
+
 
         }
     }
