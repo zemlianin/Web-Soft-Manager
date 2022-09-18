deb4061a3b7721693afce179e3d4d67e07a2a4e3
lueFlake <uselessal123@gmail.com>
2022-09-18 09:29:32 +0300

    sample controller

---
+++ APILoad/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILoad.Controllers {
+    public class SampleController : Controller {
+        static readonly string s_path = "download.txt";
+        static readonly string s_name = "download.cmd";
+
+        [HttpGet("get-app-executable")]
+        public byte[] Get(List<string> services) {
+            System.IO.File.WriteAllLines(s_path, services);
+            byte[] bytes = System.IO.File.ReadAllBytes(s_path);
+            return bytes;
+        }
+    }
+}
